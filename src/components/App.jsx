dbc2c372ee48aef8eac0356542feeac8150301c8
Pohorielova <anna.pogorielova@gmail.com>
2022-12-27 21:42:57 +0100

    Add src

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Box } from './Box';
+
+class App extends Component {
+  state = {};
+  render() {
+    return <Box as="div" p={15}></Box>;
+  }
+}
+export default App;
